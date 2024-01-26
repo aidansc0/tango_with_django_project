8718e511cf785ffa4e998f6c49004fede2e9f90a
aidansc0 <82722503+aidansc0@users.noreply.github.com>
2024-01-26 13:22:04 +0000

    Ch 4 Excercises

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,10 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
+def about(request):
+    context_dict = {'boldmessage': "This tutorial has been put together by Aidan!"}
+    return render(request, 'rango/about.html', context=context_dict)
+
 
 
 
